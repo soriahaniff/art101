5c259f5c1b68f9d36e30c2c432e8091b88fabffb
soriahaniff <146970911+soriahaniff@users.noreply.github.com>
2023-11-16 23:10:38 -0800

    lab10

---
+++ lab10/js/lab.js
@@ -0,0 +1,27 @@
+//lab.js - This simple JavaScript/jQuery script appends new elements to an output div
+// jQuery must be loaded for this script to work.
+// Author: Soriah Aniff <saniff.edu>
+// Date: November 15th 2023
+
+
+function generateRandomText() {
+  const text = "hello how are you doing I haven't seen you in such a long time I am currently living in downtown Santa Cruz, with two cats, and two roomates.";
+  const min = 3;
+  const max = 100;
+  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+  // Get a random starting index to slice the Lorem Ipsum text
+  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+  // Generate the random Lorem Ipsum-like text
+  return text.slice(randStart, randStart + randLen);
+}
+
+// click listener for button
+$("#make-convo").click(function(){
+  // get new fake dialogue
+const newText = generateRandomText();
+
+// append a new div to our output div
+$("#output").append('<div class="text"><p>' + newText +'</p></div>');
+});
+
+
